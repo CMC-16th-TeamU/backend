4ff85f3c6e813e16547bcc7e29e623ebbf3cdaf8
20210699 <20210699@duksung.ac.kr>
2024-11-24 01:03:51 +0900

    fix: 회고 api 응답 통일 (#14)

--- src/main/java/project/backend/api/regret/RegretController.java
+++ src/main/java/project/backend/api/regret/RegretController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import project.backend.api.ApiResponse;
 import project.backend.api.regret.request.MyRegretRequest;
 import project.backend.api.regret.request.RegretCreateRequest;
 import project.backend.application.regret.RegretService;
@@ -26,15 +27,15 @@ public class RegretController {
 
   @Operation(summary = "회고 등록 api")
   @PostMapping
-  public RegretCreateResponse createRegret(@Valid @RequestBody RegretCreateRequest regretCreateRequest) {
-    return regretService.createRegret(regretCreateRequest.toServiceRequest());
+  public ApiResponse<RegretCreateResponse> createRegret(@Valid @RequestBody RegretCreateRequest regretCreateRequest) {
+    return ApiResponse.OK(regretService.createRegret(regretCreateRequest.toServiceRequest()));
   }
 
-  @Operation(summary = "회고 등록 api")
+  @Operation(summary = "나의 회고 조회 api")
   @GetMapping("/my")
-  public MyRegretListResponse getMyRegrets(@Valid @RequestBody MyRegretRequest request,
+  public ApiResponse<MyRegretListResponse> getMyRegrets(@Valid @RequestBody MyRegretRequest request,
       @RequestParam(value = "page", defaultValue = "0") int page,
       @RequestParam(value = "size", defaultValue = "10") int size) {
-    return regretService.getMyRegrets(request.getUserId(), page, size);
+    return ApiResponse.OK(regretService.getMyRegrets(request.getUserId(), page, size));
   }
 }
