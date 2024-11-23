206a4d1fa615da885e8d2d9ec00231831efa77ca
junseoplee <junseoplee@soongsil.ac.kr>
2024-11-24 02:31:14 +0900

    refactor: Regret 조회 기능 수정 (#24)

--- src/main/java/project/backend/api/regret/RegretController.java
+++ src/main/java/project/backend/api/regret/RegretController.java
@@ -4,38 +4,34 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.backend.api.ApiResponse;
-import project.backend.api.regret.request.MyRegretRequest;
 import project.backend.api.regret.request.RegretCreateRequest;
 import project.backend.application.regret.RegretService;
-import project.backend.application.regret.response.MyRegretListResponse;
+import project.backend.application.regret.response.UserRegretListResponse;
 import project.backend.application.regret.response.RegretCreateResponse;
 
 @RestController
 @RequestMapping("/regrets")
 @RequiredArgsConstructor
-@Tag(name="회고 관련 기능", description = "회고 관련 API 입니다.")
+@Tag(name = "회고 관련 기능", description = "회고 관련 API 입니다.")
 public class RegretController {
 
   private final RegretService regretService;
 
-  @Operation(summary = "회고 등록 api")
+  @Operation(summary = "회고 등록 API")
   @PostMapping
-  public ApiResponse<RegretCreateResponse> createRegret(@Valid @RequestBody RegretCreateRequest regretCreateRequest) {
+  public ApiResponse<RegretCreateResponse> createRegret(
+      @Valid @RequestBody RegretCreateRequest regretCreateRequest) {
     return ApiResponse.OK(regretService.createRegret(regretCreateRequest.toServiceRequest()));
   }
 
-  @Operation(summary = "나의 회고 조회 api")
-  @GetMapping("/my")
-  public ApiResponse<MyRegretListResponse> getMyRegrets(@Valid @RequestBody MyRegretRequest request,
+  @Operation(summary = "사용자의 회고 조회 API")
+  @GetMapping("/users/{userId}")
+  public ApiResponse<UserRegretListResponse> getUserRegrets(
+      @PathVariable Long userId,
       @RequestParam(value = "page", defaultValue = "0") int page,
       @RequestParam(value = "size", defaultValue = "10") int size) {
-    return ApiResponse.OK(regretService.getMyRegrets(request.getUserId(), page, size));
+    return ApiResponse.OK(regretService.getUserRegrets(userId, page, size));
   }
 }
--- src/main/java/project/backend/api/regret/request/UserRegretRequest.java
+++ src/main/java/project/backend/api/regret/request/UserRegretRequest.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
-public class MyRegretRequest {
+public class UserRegretRequest {
+
   @NotNull(message = "회원 ID를 입력해주세요.")
   private Long userId;
 }
